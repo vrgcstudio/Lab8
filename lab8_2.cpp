6285ba312817dc89a2fd6760311bae577e0c440c
Rapepol <vrgcstudio@gmail.com>
2021-12-03 16:10:15 +0700

    lab8_2 completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,18 +1,51 @@
-//Dialog of the first example is given below.
+#include <iostream>
+using namespace std;
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+int main()
+{
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?" << endl;
+
+    cout << "?????: ";
+    string name;
+    getline(cin, name);
+
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name." << endl;
+
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << endl;
+
+    cout << name << ": ";
+    int id;
+    cin >> id;
+
+    int gen_result;
+    gen_result = (id - 120000000) / 10000000;
+
+    cout << "Fahsai: I think you may be GEAR " << gen_result << ". I have a free movie ticket for you." << endl;
+
+    cout << "Fahsai: Let's go to the cinema together!!!" << endl;
+
+    cout << "Fahsai: What movie do you want to watch?" << endl;
+
+    cin.ignore();
+
+    cout << name << ": ";
+    string movie;
+    getline(cin, movie);
+
+    cout << "Fahsai: So....which day are you free to go with me?" << endl;
+
+    cout << name << ": ";
+    string movie_date;
+    getline(cin, movie_date);
+
+    cout << "Fahsai: " << movie_date << "....that is OK!!! I'm looking forward to watching ";
+    cout << movie << " with you." << endl;
+
+    cout << name << ": ";
+    string saybye;
+    getline(cin, saybye);
+
+    cout << "Fahsai: 555+ see you " << movie_date << ". Bye Bye " << "\\" << "(^ ^)/";
+
+    return 0;
+}
